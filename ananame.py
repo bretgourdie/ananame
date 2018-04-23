fa819b4d9ce030aeee31bc7054686c2a723330d8
Bret Gourdie <bretgourdie@gmail.com>
2018-04-22 20:15:13 -0400

    Correct spacing; add files

--- ananame.py
+++ ananame.py
@@ -1,1 +1,12 @@
 
+def getName(position):
+    return input("Please enter your {} name: ".format(position))
+
+def printName(firstName, middleName, lastName):
+    print("Your name is {} {} {}".format(firstName, middleName, lastName))
+
+firstName = getName("first")
+middleName = getName("middle")
+lastName = getName("last")
+
+printName(firstName, middleName, lastName)
