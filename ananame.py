a8bc18461dfb427b8d130bc57cead264cc224fab
Bret Gourdie <bretgourdie@gmail.com>
2018-04-22 20:19:06 -0400

    Added gender prompt

--- ananame.py
+++ ananame.py
@@ -1,3 +1,5 @@
+def getAllLetters(name):
+    return fullName.replace(" ", "")
 
 def getName(position):
     return input("Please enter your {} name: ".format(position))
@@ -5,8 +7,22 @@ def getName(position):
 def printName(firstName, middleName, lastName):
     print("Your name is {} {} {}".format(firstName, middleName, lastName))
 
+def getGender():
+    gender = input("Please enter \"male\" or \"female\" if you'd like a gendered name; otherwise, enter anything else")
+    if gender == "male" or gender == "female":
+        return gender
+    else:
+        return None
+
 firstName = getName("first")
 middleName = getName("middle")
 lastName = getName("last")
 
 printName(firstName, middleName, lastName)
+fullName = firstName + " " + middleName + " " + lastName
+
+allLetters = getAllLetters(fullName)
+
+gender = getGender()
+
+
